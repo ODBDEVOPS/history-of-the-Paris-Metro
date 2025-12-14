fec667298f0746aeb4af923808a80c92e4a801e1
Onyzuka <33369346+ODBDEVOPS@users.noreply.github.com>
2025-12-14 18:54:28 +0100

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,21 @@
+// js/data.js - Données d'exemple pour 3 stations
+const allStations = [
+  {
+    "name": "Porte Maillot",
+    "openingDate": "1900-07-19",
+    "coordinates": [48.8776, 2.2849],
+    "line": "1"
+  },
+  {
+    "name": "Charles de Gaulle - Étoile",
+    "openingDate": "1900-12-01",
+    "coordinates": [48.8738, 2.2950],
+    "line": "1"
+  },
+  {
+    "name": "Trocadéro",
+    "openingDate": "1900-10-02",
+    "coordinates": [48.8630, 2.2875],
+    "line": "6"
+  }
+];
